90fbbaa45075f12b8ba61265ff0e015da0331c43
Marcos Pivetta <marcospivetta60@gmail.com>
2018-05-08 13:45:57 -0300

    + Classes ListView e ListElement

--- App.js
+++ App.js
@@ -1,17 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Header, Button, TextInput } from 'react-native';
+
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <ListView />
       </View>
     );
   }
 }
+
+class ListView extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header
+                    centerComponent={{ text: 'Listinha', style: { color: '#fff' } }}
+                />
+
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({text})}
+                    value='Título do novo item'
+                />
+                <Button
+                    onPress={onPressAddItem}
+                    title="Adicionar"
+                    color="#841584"
+                    accessibilityLabel="Adicionar"
+                />
+
+                <FlatList
+                    data={/*array de items*/}
+                    renderItem={({item}) => item}
+                />
+            </View>
+        );
+    }
+}
+
+class ListElement extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Título do item</Text>
+                <Button
+                    onPress={onPressDeleteItem}
+                    title="Deletar"
+                    color="#841584"
+                    accessibilityLabel="Deletar"
+                />
+            </View>
+        );
+    }
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
